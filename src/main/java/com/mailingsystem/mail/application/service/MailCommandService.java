bf9821983d6043c78f61c9d5bb265a81c4c56a62
Soo186 <rlagustn9797@gmail.com>
2025-05-26 16:44:37 +0900

    feat(mail): 메일 전송 유스케이스 서비스 구현
    - MailCommandService를 통해 메일 전송 흐름 구현
    - 트랜잭션 처리 및 상태 변경 포함

---
+++ src/main/java/com/mailingsystem/mail/application/service/MailCommandService.java
@@ -0,0 +1,37 @@
+package com.mailingsystem.mail.application.service;
+
+import com.mailingsystem.mail.domain.entity.Mail;
+import com.mailingsystem.mail.domain.repo.MailRepo;
+import com.mailingsystem.mail.domain.service.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MailCommandService {
+
+    private final MailRepo mailRepo;
+    private final MailSender mailSender;
+
+    @Transactional
+    public void sendMail(String to, String subject, String body) {
+        // 1. 메일 객체 생성
+        Mail mail = Mail.create(to, subject, body);
+
+        // 2. DB 저장
+        mailRepo.save(mail);
+
+        // 3. 메일 전송
+        boolean isSuccess = mailSender.send(mail);
+
+        // 4. 상태 변경
+        if (isSuccess) {
+            mail.markAsSent();
+        } else {
+            mail.markAsFailed();
+        }
+
+        // 5. 상태 변경 사항 저장
+    }
+}
