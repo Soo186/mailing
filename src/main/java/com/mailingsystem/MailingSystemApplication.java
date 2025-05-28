f5c5774b090568807d9d44c1631d42049276bbda
Soo186 <rlagustn9797@gmail.com>
2025-05-28 17:00:54 +0900

    feat : 비동기 annotation 추가

--- src/main/java/com/mailingsystem/MailingSystemApplication.java
+++ src/main/java/com/mailingsystem/MailingSystemApplication.java
@@ -2,8 +2,10 @@ package com.mailingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync //  비동기 처리 활성화
 public class MailingSystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/mailingsystem/mail/application/service/MailCommandService.java
+++ src/main/java/com/mailingsystem/mail/application/service/MailCommandService.java
@@ -4,6 +4,7 @@ import com.mailingsystem.mail.domain.entity.Mail;
 import com.mailingsystem.mail.domain.repo.MailRepo;
 import com.mailingsystem.mail.domain.service.MailSender;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ public class MailCommandService {
     private final MailRepo mailRepo;
     private final MailSender mailSender;
 
+    @Async
     @Transactional
     public void sendMail(String to, String subject, String body) {
         // 1. 메일 객체 생성
