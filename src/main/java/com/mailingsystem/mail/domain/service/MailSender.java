cc635c576938ec900ba83f42b4b891ab4938045c
Soo186 <rlagustn9797@gmail.com>
2025-05-28 17:02:14 +0900

    refactor : interface에 servcie annotation 제거

--- src/main/java/com/mailingsystem/mail/domain/service/MailSender.java
+++ src/main/java/com/mailingsystem/mail/domain/service/MailSender.java
@@ -1,9 +1,9 @@
 package com.mailingsystem.mail.domain.service;
 
 import com.mailingsystem.mail.domain.entity.Mail;
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public interface MailSender {
     boolean send(Mail mail);
 }
