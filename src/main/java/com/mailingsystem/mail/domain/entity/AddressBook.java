697090040b6bcba1181473fd723e847eac452006
Soo186 <rlagustn9797@gmail.com>
2025-05-30 16:26:31 +0900

    refactor : jpa 설정 제거

--- src/main/java/com/mailingsystem/mail/domain/entity/AddressBook.java
+++ src/main/java/com/mailingsystem/mail/domain/entity/AddressBook.java
@@ -1,17 +1,11 @@
 package com.mailingsystem.mail.domain.entity;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
+
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AddressBook {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name; // 수신자 이름
@@ -21,6 +15,15 @@ public class AddressBook {
     //  정적 팩토리 메서드 사용
     public static AddressBook create(String name, String email) {
         AddressBook addressBook = new AddressBook();
+        addressBook.name = name;
+        addressBook.email = email;
+        return addressBook;
+    }
+    // 조회용 정적 팩토리
+
+    public static AddressBook restore(Long id, String name, String email) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.id = id;
         addressBook.name = name;
         addressBook.email = email;
         return addressBook;
--- src/main/java/com/mailingsystem/mail/domain/entity/Mail.java
+++ src/main/java/com/mailingsystem/mail/domain/entity/Mail.java
@@ -1,31 +1,18 @@
 package com.mailingsystem.mail.domain.entity;
 
 import com.mailingsystem.mail.domain.enums.MailStatus;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Mail {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String toAddress;
-
     private String subject;
-
-    @Lob
     private String body;
-
-    @Enumerated(EnumType.STRING)
     private MailStatus status;
 
-    // 정적 팩토리 메서드 (생성 책임 부여)
+    // 정적 팩토리 메서드
     public static Mail create(String toAddress, String subject, String body) {
         Mail mail = new Mail();
         mail.toAddress = toAddress;
@@ -35,7 +22,18 @@ public class Mail {
         return mail;
     }
 
-    // 상태 전이 메서드 (도메인 로직)
+    // 조회용 정적 팩토리
+    public static Mail restore(Long id, String to, String subject, String body, MailStatus status) {
+        Mail mail = new Mail();
+        mail.id = id;
+        mail.toAddress = to;
+        mail.subject = subject;
+        mail.body = body;
+        mail.status = status;
+        return mail;
+    }
+
+    // 상태 전이
     public void markAsSent() {
         this.status = MailStatus.SENT;
     }
