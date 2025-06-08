d69e301acb4a0f83c49313c9151e5e6111fa37fd
Soo186 <rlagustn9797@gmail.com>
2025-06-08 18:34:32 +0900

    feat : 폼 바인딩 위해 setter 선택 (추후 지양하는 쪽으로)

--- src/main/java/com/mailingsystem/mail/api/dto/MailRequest.java
+++ src/main/java/com/mailingsystem/mail/api/dto/MailRequest.java
@@ -2,9 +2,15 @@ package com.mailingsystem.mail.api.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MailRequest {
 
     @Email
